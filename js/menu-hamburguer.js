729c6003040e2f7c03d05aff52946917c2a3cade
Gabrielle Rodrigues <gabriellerut@gmail.com>
2022-05-24 15:21:33 -0300

    Criando funcionalidade ao clicar no menu.

--- js/menu-hamburguer.js
+++ js/menu-hamburguer.js
@@ -1,3 +1,28 @@
-const menuHamburguer = document.querySelector('#menu-hamburguer')
+const menuHamburguer = document.querySelector('#mobile-menu')
 
-menuHamburguer.addEventListener('click', () => console.log('teste'))
+const menuOptions = document.querySelector('#menu-options')
+
+
+const showMenu = () => {
+    menuOptions.classList.toggle('show')
+}
+
+let menuOpen = false
+
+const toggleHamburguer = () => {
+    if(!menuOpen){
+        menuHamburguer.classList.add('open')
+        menuOpen = true
+    } else {
+        menuHamburguer.classList.remove('open')
+        menuOpen = false
+    }
+
+}
+
+const handleClick = () => {
+    showMenu()
+    toggleHamburguer()
+}
+
+menuHamburguer.addEventListener('click', () => handleClick())
